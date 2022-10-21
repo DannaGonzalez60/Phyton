524521417f45bfd2ac5de21cfc73002ada267642
APRENDIZ <APRENDIZ@BOGAPRCSFFS275.FormacionSena.RED>
2022-10-21 14:26:11 -0500

    HaKtUaLiSaCYon

--- jstore/jstore/views.py
+++ jstore/jstore/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 def index(request):
     return render (request, 'index.html', {
-        #contexto
+        'variable' : 'producto',
+        'pro1' : 'loro'
+    })
+
+def contact(request):
+    return render(request, 'contact.html', {
+        
     })
