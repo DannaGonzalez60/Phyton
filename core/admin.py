c699819134238cd6e963b83b21516840ff7a3f77
DannaGonzalez60 <99436253+DannaGonzalez60@users.noreply.github.com>
2022-10-27 21:44:51 -0500

    Delete admin.py

--- core/admin.py
+++
@@ -1,7 +0,0 @@
-from itertools import product
-from django.contrib import admin
-from .models import Categoria, Marca, Product
-# Register your models here.
-admin.site.register(Categoria)
-admin.site.register(Marca)
-admin.site.register(Product)
